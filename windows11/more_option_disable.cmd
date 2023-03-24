6aa0fc62f67b8a754b7417f0fb68e6f5d52dc855
Heon-Jin Ha <60867281+heonha@users.noreply.github.com>
2023-03-24 16:51:47 +0900

    Update more_option_disable.cmd
     설명 추가

--- windows11/more_option_disable.cmd
+++ windows11/more_option_disable.cmd
@@ -1,7 +1,11 @@
 @echo off
 chcp 1252
 
-echo processing... If it takes too long, press enter. 
+echo Bring back left-click Windows 10 right-click menu!
+echo .
+echo .
+echo processing... If it takes too long time, close and get administrator auth.  
+
 
 :: add registry
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
